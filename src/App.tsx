f0d23d94f54d9651458558c1ac349d99a819a327
Vynicius <vynicius.henrique15@gmail.com>
2025-09-24 14:48:57 -0300

    adicionando rota padrao para url invalida

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import { FirstAccess } from "./Pages/FirstAccess/FirstAccess";
 
 export function App() {
   return (
-    <FirstAccess/>
+    <Routes>
+      <Route path="/" element={<FirstAccess/>}/>
+      <Route path="*" element={<h1>Página Não Encontrada - Erro 404</h1>} />
+    </Routes>
   )
 }
 
