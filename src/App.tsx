2f1f0ed5a237ae22a1c8d6888b41fdcbfc6c6b98
Devalice-braz <alicebraz094@gmail.com>
2025-09-25 11:53:23 -0300

    Adicionando animação de inicialização do site com a logo animada

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { FirstAccess } from "./Pages/FirstAccess/FirstAccess";
+import SplashScreen from "./Pages/FirstAccess/SplashScreen";
 
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<FirstAccess/>}/>
+      <Route path="/" element={<SplashScreen onFinish={() => window.location.replace("/FirstAccess")} />} />
+      <Route path="/FirstAccess" element={<FirstAccess />} />
       <Route path="*" element={<h1>Página Não Encontrada - Erro 404</h1>} />
     </Routes>
   )
--- src/Pages/FirstAccess/FirstAccess.tsx
+++ src/Pages/FirstAccess/FirstAccess.tsx
@@ -3,29 +3,29 @@ import './FirstAccess.css';
 
 export function FirstAccess() {
 	return (
-        <Container className="d-flex align-items-center justify-content-center vh-100">
-            <Card className="card p-4 d-flex align-items-center">
+			<Container className="d-flex align-items-center justify-content-center vh-100">
+				<Card className="card p-4 d-flex align-items-center">
 
-                <h1 className="title-style">Acesso do Bombeiro</h1>
-				<p className="text-body">
-					Não acessou nenhuma vez o <b>SIOB-PE</b>? <br />
-					Faça seu <b>cadastro</b> clicando <br />
-					em <b>primeiro acesso.</b>
-				</p>
+					<h1 className="title-style">Acesso do Bombeiro</h1>
+					<p className="text-body">
+						Não acessou nenhuma vez o <b>SIOB-PE</b>? <br />
+						Faça seu <b>cadastro</b> clicando <br />
+						em <b>primeiro acesso.</b>
+					</p>
 
-				<Button className="button-first-access">
-					Primeiro Acesso
-				</Button>
+					<Button className="button-first-access">
+						Primeiro Acesso
+					</Button>
 
-				<p className="text-body mt-4">
-					Já fez o seu primeiro acesso?<br />
-					Clique em entrar.
-				</p>
+					<p className="text-body mt-4">
+						Já fez o seu primeiro acesso?<br />
+						Clique em entrar.
+					</p>
 
-				<Button className="button-enter">
-					Entrar
-				</Button>
-            </Card>
-        </Container>
-	);
-}
+					<Button className="button-enter">
+						Entrar
+					</Button>
+				</Card>
+			</Container>
+			);
+} 
