3e183fa29a26952979604c4f3610eb42bfc89397
Vynicius <vynicius.henrique15@gmail.com>
2025-10-06 20:13:52 -0300

    componentizando o layout do formulario

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { FirstAccess } from "./Pages/FirstAccess/FirstAccess";
 import SplashScreen from "./Pages/FirstAccess/SplashScreen";
 import { Register } from "./Pages/Register/Register";
+import { AuthCardLayout } from "./Components/AuthCardLayout/AuthCardLayout";
 
 export function App() {
   return (
@@ -9,6 +10,7 @@ export function App() {
       <Route path="/" element={<SplashScreen onFinish={() => window.location.replace("/FirstAccess")} />} />
       <Route path="/FirstAccess" element={<FirstAccess />} />
       <Route path="/Register" element={<Register/>} />
+      <Route path="/test" element={<AuthCardLayout/>}/>
       <Route path="*" element={<h1>Página Não Encontrada - Erro 404</h1>} />
     </Routes>
   )
