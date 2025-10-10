a78a06e424ccce8e05fda809214b934e0553b934
Vynicius <vynicius.henrique15@gmail.com>
2025-10-10 16:43:05 -0300

    criando componente navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import SplashScreen from "./Pages/SplashScreen/SplashScreen";
 import { Register } from "./Pages/Login/Login";
+import { CustomNavBar } from "./Components/CustomNavBar/CustomNavbar";
 
 
 export function App() {
@@ -8,6 +9,7 @@ export function App() {
     <Routes>
       <Route path="/" element={<SplashScreen onFinish={() => window.location.replace("/Login")} />} />
       <Route path="/Login" element={<Register />} />
+      <Route path="/test" element={<CustomNavBar />} />
       <Route path="*" element={<h1>Página Não Encontrada - Erro 404</h1>} />
     </Routes>
   )
---
+++ src/Components/CustomNavBar/CustomNavbar.tsx
@@ -0,0 +1,79 @@
+import { Navbar, Offcanvas } from "react-bootstrap";
+import "./CustomNavBar.css";
+import { useState } from "react";
+import { ChartBarIcon, FileMagnifyingGlassIcon, FileTextIcon, GearSixIcon, ListIcon, PencilSimpleIcon, UserCircleIcon, UsersThreeIcon, XIcon } from "@phosphor-icons/react";
+
+type props = {
+  nameMilitary: string;
+} 
+export function CustomNavBar( {nameMilitary}: props) {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <Navbar className="navbar-style">
+      <div className="content-navbar-style">
+        <ListIcon className="list-icon-style" onClick={handleShow} />
+        <a href="">
+          {nameMilitary}
+          <UserCircleIcon className="ms-3" size={41} color="#000000"/>
+        </a>
+      </div>
+
+      <Offcanvas show={show} onHide={handleClose}>
+
+        <Offcanvas.Header className="d-flex justify-content-end align-ites-center">
+          <XIcon weight="bold" onClick={handleClose} className="x-icon-style" />
+        </Offcanvas.Header>
+
+        <hr className="divider-style" />
+
+        <Offcanvas.Body className="d-flex align-items-start flex-column w-100">
+
+          <div className="content-div-style">
+            <a href="/">
+              <FileTextIcon size={33} className="me-3" />
+              Relatórios e expotação
+            </a>
+          </div>
+
+          <div className="content-div-style">
+            <a href="/">
+              <UsersThreeIcon size={33} className="me-3" />
+              Gestão de usuários
+            </a>
+          </div>
+
+          <div className="content-div-style">
+            <a href="/">
+              <FileMagnifyingGlassIcon size={33} className="me-3" />
+              Auditoria e Logs
+            </a>
+          </div>
+
+          <div className="content-div-style">
+            <a href="/">
+              <ChartBarIcon size={33} className="me-3" />
+              Dashboard operacional
+            </a>
+          </div>
+
+          <div className="content-div-style">
+            <a href="/">
+              <PencilSimpleIcon size={33} className="me-3" />
+              Criação/Edição de campos do formulário
+            </a>
+          </div>
+
+            <div className="content-div-style">
+            <a href="/">
+              <GearSixIcon size={33} className="me-3" />
+              Configurações
+            </a>
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </Navbar>
+  );
+}
