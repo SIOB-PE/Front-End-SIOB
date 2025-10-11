f7cc312126aa13dfdb6729b47541c01e958670ff
Vynicius <vynicius.henrique15@gmail.com>
2025-10-11 01:03:41 -0300

    finalizando navbar

--- src/Components/CustomNavBar/CustomNavbar.tsx
+++ src/Components/CustomNavBar/CustomNavbar.tsx
@@ -1,12 +1,23 @@
 import { Navbar, Offcanvas } from "react-bootstrap";
 import "./CustomNavBar.css";
 import { useState } from "react";
-import { ChartBarIcon, FileMagnifyingGlassIcon, FileTextIcon, GearSixIcon, ListIcon, PencilSimpleIcon, UserCircleIcon, UsersThreeIcon, XIcon } from "@phosphor-icons/react";
+import {
+  ChartBarIcon,
+  FileMagnifyingGlassIcon,
+  FileTextIcon,
+  GearSixIcon,
+  ListIcon,
+  PencilSimpleIcon,
+  SignOutIcon,
+  UserCircleIcon,
+  UsersThreeIcon,
+  XIcon,
+} from "@phosphor-icons/react";
 
 type props = {
   nameMilitary: string;
-} 
-export function CustomNavBar( {nameMilitary}: props) {
+};
+export function CustomNavBar({ nameMilitary }: props) {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -17,12 +28,11 @@ export function CustomNavBar( {nameMilitary}: props) {
         <ListIcon className="list-icon-style" onClick={handleShow} />
         <a href="">
           {nameMilitary}
-          <UserCircleIcon className="ms-3" size={41} color="#000000"/>
+          <UserCircleIcon className="ms-3" size={41} color="#000000" />
         </a>
       </div>
 
       <Offcanvas show={show} onHide={handleClose}>
-
         <Offcanvas.Header className="d-flex justify-content-end align-ites-center">
           <XIcon weight="bold" onClick={handleClose} className="x-icon-style" />
         </Offcanvas.Header>
@@ -30,7 +40,6 @@ export function CustomNavBar( {nameMilitary}: props) {
         <hr className="divider-style" />
 
         <Offcanvas.Body className="d-flex align-items-start flex-column w-100">
-
           <div className="content-div-style">
             <a href="/">
               <FileTextIcon size={33} className="me-3" />
@@ -66,13 +75,21 @@ export function CustomNavBar( {nameMilitary}: props) {
             </a>
           </div>
 
-            <div className="content-div-style">
+          <div className="content-div-style">
             <a href="/">
               <GearSixIcon size={33} className="me-3" />
               Configurações
             </a>
           </div>
+          
+        <div className="d-flex align-items-center justify-content-center w-100 mt-3">
+          <a href="" className="sign-out-style">
+            Sair
+            <SignOutIcon className="ms-2" size={20} weight="bold"/>
+          </a>
+        </div>
         </Offcanvas.Body>
+
       </Offcanvas>
     </Navbar>
   );
