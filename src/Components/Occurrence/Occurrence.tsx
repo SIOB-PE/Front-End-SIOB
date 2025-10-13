deecc2d530c473060b61a9e8e39c4d240a4041e7
Vynicius <vynicius.henrique15@gmail.com>
2025-10-13 11:29:34 -0300

    criando componentizando o item que contem a ocorrencia

---
+++ src/Components/Occurrence/Occurrence.tsx
@@ -0,0 +1,35 @@
+import { Card, Col, Container, Row } from "react-bootstrap";
+import "./Occurrence.css";
+import { ArrowUpRightIcon } from "@phosphor-icons/react";
+
+export function Occurrence() {
+
+
+  const handleCardClick = () => {
+    alert("Você clicou no card!");
+  };
+  
+  return (
+    <Container fluid className="mt-5">
+      <Row>
+        <Col md={5}>
+          <Card onClick={handleCardClick} className="card-occurrence">
+            <Card.Body className="card-body-occurrence">
+              <div className="div-card-body-occurrence">
+                <h4>Incendio Residencia</h4>
+                <div className="d-flex align-items-center justify-content-start">
+                  <p>Local:</p>
+                  <p>Rua das flores, 123 - Boa viagem |</p>
+                  <p>Hora:</p>
+                  <p>14:34</p>
+                </div>
+              </div>
+
+              <ArrowUpRightIcon size={37} weight="bold" />
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
