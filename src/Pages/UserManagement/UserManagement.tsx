bf3bd6ccbe2fa3c4a1e91fb74bd2600bc94b902d
Vynicius <vynicius.henrique15@gmail.com>
2025-10-20 13:02:09 -0300

    adicionando fetch de pesquisa

--- src/Pages/UserManagement/UserManagement.tsx
+++ src/Pages/UserManagement/UserManagement.tsx
@@ -15,8 +15,60 @@ import {
   PlusIcon,
   UserCircleMinusIcon,
 } from "@phosphor-icons/react";
+import { useEffect, useState } from "react";
+
+
+
+interface Usuario {
+  id: string;
+  matricula: string;
+  nome: string;
+  email: string;
+  dataNascimento: string;
+  senha?: string;
+  tipoUsuario: string;
+}
 
 export function UserManagement() {
+  const [searchNome, setSearchNome] = useState("");
+  const [users, setUsers] = useState<Usuario[]>([]);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/usuario");
+
+      if (!response.ok) {
+        throw new Error(`Erro HTTP! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/usuario?nome=${searchNome}`
+      );
+
+      if (!response.ok) {
+        throw new Error(`Erro HTTP! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error("Erro ao pesquisar usuários:", error);
+    }
+  };
+
   return (
     <>
       <CustomNavBar nameMilitary="Major Silva" />
@@ -29,7 +81,10 @@ export function UserManagement() {
                 <Row className="w-100">
                   <Col md={5}>
                     <div className="w-100 h-100 d-flex align-items-center justify-content-start">
-                      <Button href="/RegisterUser" className="user-management-new-user-button-style">
+                      <Button
+                        href="/RegisterUser"
+                        className="user-management-new-user-button-style"
+                      >
                         <PlusIcon className="me-2" size={30} />
                         Novo usuário
                       </Button>
@@ -41,8 +96,13 @@ export function UserManagement() {
                       <FormControl
                         placeholder="Buscar usuários"
                         className="list-occurrence-form-control-style"
+                        value={searchNome}
+                        onChange={(e) => setSearchNome(e.target.value)}
                       />
-                      <Button className="list-occurrence-button-style">
+                      <Button
+                        onClick={handleSearch}
+                        className="list-occurrence-button-style"
+                      >
                         <MagnifyingGlassIcon size={24} />
                       </Button>
                     </InputGroup>
@@ -73,100 +133,41 @@ export function UserManagement() {
                     <h5>Ações</h5>
                   </Col>
                 </Row>
-                <hr />
-                <Row className="d-flex align-items-center justify-content-between my-4">
-                  <Col>
-                    <p>Joao Silva</p>
-                  </Col>
-                  <Col>
-                    <p>jsilva@cbmpe.gov.br</p>
-                  </Col>
-                  <Col>
-                    <p>Administrador</p>
-                  </Col>
-                  <Col>
-                    <p>2024-01-15 14:30</p>
-                  </Col>
-                  <Col>
-				    <Button className="user-management-edit-button-style me-4">
-                      <PencilSimpleIcon size={30} />
-                    </Button>
-                    <Button className="user-management-delete-button-style">
-                      <UserCircleMinusIcon size={30} />
-                    </Button>
-                  </Col>
-                </Row>
 
                 <hr />
-                <Row className="d-flex align-items-center justify-content-between my-4">
-                  <Col>
-                    <p>Pedro</p>
-                  </Col>
-                  <Col>
-                    <p>psilva@cbmpe.gov.br</p>
-                  </Col>
-                  <Col>
-                    <p>Bombeiro</p>
-                  </Col>
-                  <Col>
-                    <p>2024-01-15 14:30</p>
-                  </Col>
-                  <Col>
-				    <Button className="user-management-edit-button-style me-4">
-                      <PencilSimpleIcon size={30} />
-                    </Button>
-                    <Button className="user-management-delete-button-style">
-                      <UserCircleMinusIcon size={30} />
-                    </Button>
-                  </Col>
-                </Row>
-
-                <hr />
-                <Row className="d-flex align-items-center justify-content-between my-4">
-                  <Col>
-                    <p>Julia</p>
-                  </Col>
-                  <Col>
-                    <p>jsilva@cbmpe.gov.br</p>
-                  </Col>
-                  <Col>
-                    <p>Analista</p>
-                  </Col>
-                  <Col>
-                    <p>2024-01-15 14:30</p>
-                  </Col>
-                  <Col>
-				    <Button className="user-management-edit-button-style me-4">
-                      <PencilSimpleIcon size={30} />
-                    </Button>
-                    <Button className="user-management-delete-button-style">
-                      <UserCircleMinusIcon size={30} />
-                    </Button>
-                  </Col>
-                </Row>
-                <hr />
-                <Row className="d-flex align-items-center justify-content-between my-4">
-                  <Col>
-                    <p>Maria </p>
-                  </Col>
-                  <Col>
-                    <p>msilva@cbmpe.gov.br</p>
-                  </Col>
-                  <Col>
-                    <p>Administrador</p>
-                  </Col>
-                  <Col>
-                    <p>2024-01-15 14:30</p>
-                  </Col>
-                  <Col>
-				    <Button className="user-management-edit-button-style me-4">
-                      <PencilSimpleIcon size={30} />
-                    </Button>
-                    <Button className="user-management-delete-button-style">
-                      <UserCircleMinusIcon size={30} />
-                    </Button>
-                  </Col>
-                </Row>
+                {users.length > 0 ? (
+                  users.map((user) => (
+                    <div key={user.id}>
+                      <Row className="d-flex align-items-center justify-content-between my-4">
+                        <Col>
+                          <p>{user.nome}</p>
+                        </Col>
+                        <Col>
+                          <p>{user.email}</p>
+                        </Col>
+                        <Col>
+                          <p>{user.tipoUsuario}</p>
+                        </Col>
+                        <Col>
+                          <p>Ultimo acesso</p>
+                        </Col>
+                        <Col>
+                          <Button className="user-management-edit-button-style me-4">
+                            <PencilSimpleIcon size={30} />
+                          </Button>
+                          <Button className="user-management-delete-button-style">
+                            <UserCircleMinusIcon size={30} />
+                          </Button>
+                        </Col>
+                      </Row>
+                      <hr />
+                    </div>
+                  ))
+                ) : (
+                  <Row className="text-center my-4">
+                    <p>Nenhum usuário encontrado.</p>
+                  </Row>
+                )}
               </Card.Body>
             </Card>
           </Col>
